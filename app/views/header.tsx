b8f91990ac2fd3a43c42be26de6ff07d7a63e73b
Ben Nolan <bnolan@gmail.com>
2023-06-21 11:29:33 +1200

    Link to github

--- app/views/header.tsx
+++ app/views/header.tsx
@@ -1,7 +1,9 @@
 export function Header () {
   return (
     <header>
-      <a class='logo' href="/"><img src="/logo.svg" />yofolk</a>
+      <b><a class='logo' href="/"><img src="/logo.svg" />yofolk</a></b> |
+      {' '}<a href="https://github.com/bnolan/yofolk-node">Github</a> |
+      {' '}<small>Yofolk is a experiment in social network on ethereum</small>
     </header>
 
   )
