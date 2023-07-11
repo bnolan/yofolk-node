cd5c61b3b66c5e6289344dd6f8b1331a237c9dc3
Ben Nolan <bnolan@gmail.com>
2023-07-11 18:47:26 +1200

    Shorten links

--- app/components/summary.tsx
+++ app/components/summary.tsx
@@ -6,12 +6,13 @@ export default function Summary (props: { users: UserCache, post: PostRecord }) 
   let { post, users } = props
   
   let url = `/p/${post.id}`
+  let content = post.content.replace(/(https:\/\/\S+)/, f => f.slice(0, 40) + (f.length > 40 ? '...' : ''))
 
   return (
     <div class='post'>
       <x-icon wallet={post.author} />
       <small class='meta'><a href={url}><Author author={post.author} users={users} /></a></small>
-      <cite>{post.content}</cite>
+      <cite>{content}</cite>
     </div>
   )
 }
